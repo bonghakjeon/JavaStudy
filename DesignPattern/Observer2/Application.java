e835409530c0028f9e5de323960b47c984ee6631
DESKTOP-GUHF3TT\bhjeon <bhjeon@imbu.co.kr>
2024-06-19 09:21:43 +0900

    [자바 디자인 패턴 이해] 스터디 15강 옵저버 패턴 (Observer) - 2 커밋

---
+++ DesignPattern/Observer2/Application.java
@@ -0,0 +1,30 @@
+// [자바 디자인 패턴 이해] 스터디
+// 15강 옵저버 패턴 (Observer) - 2
+// 유튜브 참고 URL - 
+// https://youtu.be/6p0kD0LkaIs?si=ynw8-CSVm-pyMJrC
+
+// 블로그 참고 URL - https://velog.io/@ljo_0920/%EC%98%B5%EC%A0%80%EB%B2%84-%ED%8C%A8%ED%84%B4-xzrb4hge
+
+package DesignPattern.Observer2;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Application {
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        // addObserver - 옵저버 추가 
+        button.addObserver(new Observer() {
+            @override
+            public void update(Observable o, Object arg) {
+                System.out.println(o+"is Clicked");
+            }
+        });
+
+        button.onClick();
+        button.onClick();
+        button.onClick();
+    }
+}
+
